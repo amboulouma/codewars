0013992024884d0d4d0bea261b58696cd2f57f00
Amine M. Boulouma <amine.boulouma@flambok.com>
2018-04-04 21:52:56 +0200

    added 3kyu, 4kyu and 8kyu codes

---
+++ codewars/8kyu/decending_order.py
@@ -0,0 +1,4 @@
+def Descending_Order(num):
+    return int(str(num)[::-1])
+
+print(Descending_Order(0112))
---
+++ codewars/8kyu/sorting_numbers.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    if nums != [] and nums != None : return sorted(nums)
+    else: return []
+    
+def solution(nums):
+    return sorted(nums) if nums else []
+
+solution = lambda l: sorted(l) if l else []
